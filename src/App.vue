4022091182820459ee3684114df6a6180f848b3b
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-10-03 14:37:31 +0700

    :fire: Remove responsive menu view

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,6 @@
       <div class="flex items-center flex-shrink-0 text-white mr-6">
         <img class="h-16 w-full" src="@/assets/logo_transparent_crop.png">
       </div>
-      <div class="block lg:hidden">
-        <button
-          class="flex items-center px-3 py-2 border rounded text-orange-200 border-orange-400 hover:text-white hover:border-white"
-        >
-          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
       <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
           <a
@@ -24,10 +14,6 @@
             href="#responsive-header"
             class="block mt-4 lg:inline-block lg:mt-0 text-orange-200 hover:text-white mr-4"
           >Examples</a>
-          <a
-            href="#responsive-header"
-            class="block mt-4 lg:inline-block lg:mt-0 text-orange-200 hover:text-white"
-          >Blog</a>
         </div>
       </div>
     </nav>
