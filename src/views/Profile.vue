4be91f0efbc5461775c25319e5b84a80a3674b59
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-10-03 14:53:43 +0700

    :sparkles: Add views component

---
+++ src/views/Profile.vue
@@ -0,0 +1,8 @@
+<template></template>
+<style lang="scss" scoped>
+</style>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class Profile extends Vue {}
+</script>
