f53ed47adf51f77e89925397ca03dc75f9266f2f
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-10-03 14:33:43 +0700

    :sparkles: Add vue axios for making request

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAxios, axios.create({
+  baseURL: process.env.VUE_API_BASE_URL,
+}));
 
 new Vue({
   router,
