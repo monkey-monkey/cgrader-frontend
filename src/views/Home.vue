f41805b5010c5a62789be65c364916877bcc675f
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-10-03 12:55:56 +0700

    :sparkles: Add navigation bar and logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,6 @@
 <template>
   <div>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Button</button>
-    <button
-      class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-    >Button</button>
-    <button
-      class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-    >Button</button>
-    <button
-      class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
-    >
-      <svg
-        class="fill-current w-4 h-4 mr-2"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-      >
-        <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-      </svg>
-      <span>Download</span>
-    </button>
+    <h1>This is home page</h1>
   </div>
 </template>
 
