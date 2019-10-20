39f9e44c52fac9a7ece7944dde5ebb7675289ba3
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-10-20 08:28:30 +0700

    :art: Fix nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
   <div id="app">
-    <nav class="flex items-center justify-between flex-wrap bg-orange-700 p-3">
-      <div class="flex items-center flex-shrink-0 text-white mr-6">
-        <img class="h-16 w-full" src="@/assets/logo_transparent_crop.png">
-      </div>
-      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm lg:flex-grow">
-          <a
-            href="#responsive-header"
-            class="block mt-4 lg:inline-block lg:mt-0 text-orange-200 hover:text-white mr-4"
-          >Docs</a>
-        </div>
+    <nav class="flex items-center bg-orange-700 p-3">
+      <img class="h-16 pr-3" src="@/assets/logo_transparent_crop.png">
+      <div class="flex-1 flex justify-around">
+        <router-link class="text-white hover:text-orange-300" to="/" >Docs</router-link>
+        <router-link class="text-white hover:text-orange-300" to="/" >Docs</router-link>
+        <router-link class="text-white hover:text-orange-300" to="/" >Docs</router-link>
       </div>
     </nav>
     <router-view />
