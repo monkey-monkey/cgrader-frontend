f7df04218f0ade5a8593bc3d8cfa1ee3fbbd1481
UtopiaBeam <utopiabeam@gmail.com>
2019-10-26 08:46:55 +0700

    :sparkles: :construction: Add problem page

--- src/types/problems.ts
+++ src/types/problems.ts
@@ -1,8 +1,14 @@
 export interface IProblem {
+    _id: string;
     name: string;
     code: string;
     timeLimit: number;
     memLimit: number;
     testCases: number;
-    fileUrl?: string;
+    problemUrl?: string;
+}
+
+export interface FileDetail {
+    contentType: string;
+    signedUrl: string;
 }
