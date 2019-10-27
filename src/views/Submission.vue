0e72c0f8900798b55248d6ba789601582a66fc55
UtopiaBeam <utopiabeam@gmail.com>
2019-10-27 08:10:35 +0700

    :sparkles: :construction: Add code submission functionality

---
+++ src/views/Submission.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>This is submission</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Submission extends Vue {
+  async mounted() {
+    const response = await this.axios.get("submission");
+  }
+}
+</script>
