86541d78690c2ee404119ff502c8830632f0e58c
UtopiaBeam <utopiabeam@gmail.com>
2019-11-10 10:04:17 +0700

    :sparkles: Add logout button

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,12 @@
     <nav class="flex items-center bg-orange-700 p-3">
       <img class="h-12 pr-3" src="@/assets/logo_transparent_crop.png" />
       <div class="flex-1 flex justify-around">
-        <router-link class="text-white hover:text-orange-300" to="/" >Home</router-link>
-        <router-link class="text-white hover:text-orange-300" to="/problems" >Problems</router-link>
+        <router-link class="text-white hover:text-orange-300" to="/">Home</router-link>
+        <router-link class="text-white hover:text-orange-300" to="/problems">Problems</router-link>
         <router-link class="text-white hover:text-orange-300" to="/submissions">Submissions</router-link>
+        <!-- <div v-if="loggedIn()"> -->
+          <Logout />
+        <!-- </div> -->
       </div>
     </nav>
     <router-view />
@@ -15,3 +18,20 @@
 <style lang="scss">
 @import "@/assets/css/tailwind.scss";
 </style>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import Logout from "@/components/Logout.vue";
+
+@Component({
+  components: {
+    Logout
+  }
+})
+export default class App extends Vue {
+  private loggedIn() {
+    console.log(Vue.cookies.get("authToken"));
+    return Vue.cookies.get("authToken");
+  }
+}
+</script>
---
+++ src/components/Logout.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    class="bg-transparent hover:bg-white text-white font-semibold hover:text-orange-700 py-2 px-4 border border-white hover:border-orange-700 rounded"
+    @click="logout()"
+  >Logout</button>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+@Component
+export default class Logout extends Vue {
+  private logout() {
+    Vue.cookies.remove('authToken');
+    location.reload();
+  }
+}
+</script>
